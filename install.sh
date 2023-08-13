185fea2ac70654e2efdc77e6cb2f8d5f0c6ff0bd
Exiles.prx <exiles.prx@gmail.com>
2023-08-13 19:28:24 -0400

    Removing sudo
    Run script as sudo

--- install.sh
+++ install.sh
@@ -4,19 +4,19 @@ fonts=("3270" "FiraCode" "Hack" "IntelOneMono" "JetBrainsMono" "Meslo" "SpaceMon
 version=3.0.2
 
 # Install deps
-which wget | sudo apt -y install wget
-which unzip | sudo apt -y install unzip
-which curl | sudo apt -y install curl
+which wget | apt -y install wget
+which unzip | apt -y install unzip
+which curl | apt -y install curl
 
 # Download fonts
 for value in "${fonts[@]}"
 do
 	wget -O /home/$USER/Downloads/$value.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v$version/$value.zip
-	sudo unzip /home/$USER/Downloads/$value.zip -d /usr/local/share/fonts/$value/
+	unzip /home/$USER/Downloads/$value.zip -d /usr/local/share/fonts/$value/
 done
-sudo fc-cache -v
+fc-cache -v
 
 # Install starship
-sudo curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | sh
 echo 'eval "$(starship init bash)"' >> /home/$USER/.bashrc
 starship preset nerd-font-symbols -o /home/$USER/.config/starship.toml
