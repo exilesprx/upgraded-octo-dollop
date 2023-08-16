bce12c9f8191a4f4338c5866af542a80c9b786f8
Exiles.prx <exiles.prx@gmail.com>
2023-08-15 21:35:09 -0400

    Changing script to only install fonts

---
+++ install-fonts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+fonts=("3270" "FiraCode" "Hack" "IntelOneMono" "JetBrainsMono" "Meslo" "SpaceMono")
+deps=("wget" "unzip" "curl")
+version=3.0.2
+
+# Install deps
+index=0
+for dep in "${deps[@]}"
+do
+	if which $dep; then
+		unset 'deps[index]'
+	fi
+	let index++
+done
+
+if [[ ${#deps[@]} != 0 ]]; then
+	echo "Missing deps ${deps[@]}"
+	exit 1
+fi
+
+# Download fonts
+for value in "${fonts[@]}"
+do
+	wget -O $HOME/Downloads/$value.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v$version/$value.zip
+	unzip $HOME/Downloads/$value.zip -d /usr/local/share/fonts/$value/
+done
+fc-cache -v
